a0d755e1e578186f4b1edb67e1755e07917e92ab
Andreas Giemza <andreas@giemza.net>
2012-06-14 03:52:10 +0200

    v0.1.0 - first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# visual-diffs-for-eagle-and-git
+
+## Information
+If you are working on an [CadSoft EAGLE](http://www.cadsoftusa.com/eagle-pcb-design-software/) project with a team and you are tracking your progress with [Git](http://git-scm.com/), this little programm can help you to see what your team has changed. It makes an diff image which shows the changes on the schematics and boards between the commits.
+
+But first some notes:
+* This my first ruby programm. It is very, very ugly written.
+* It's very slow! For every diff image it takes about one minute. But when only one commit is new, it only makes diff images for the new commit.
+* Sorry for my bad English!
+* Only tested it under Windows.
+* **Every help is welcome and appreciated!**
+
+You can test it with the [testrepo](https://github.com/hurik/visual-diffs-for-eagle-and-git_testrepo).
+
+This little programm based on [eagle-converter](https://gitorious.org/gitedaous/eagle-converter) by Patrick Franken.
+He made the countSheets.ulp file and most of the code is from him, but i didn't get his code running and so I made my own programm.
+
+
+# Requirements
+**grit**
+> gem install grit
+
+Under windows you need the [Development Kit](https://github.com/oneclick/rubyinstaller/wiki/Development-Kit).
+
+**chunky_png**
+> gem install chunky_png
+
+
+## Usage
+1. Open the vdfeag.rb file and set the repoPath, repoBranch and @eaglePath.
+1. Start the file!
+1. After some time your find the diff images under images/diff.
+
+
+## TODO
+* Improve code
+* Improve speed
+* Improve folder structure
+* Improve file naming
+* Improve diff images
+    * Removed elements should be red
+    * Added elements should be green
+    * Unchanged elements should be shown with half opaque 
+    * Board diffs are very ugly ...
+* Improve comments
+* Improve programm output
+* Improve everything ...
+* Add a gui
+
+
+## Changelog
+### v0.1.0
+* First commit
---
+++ vdfeag.rb
@@ -0,0 +1,162 @@
+# visual-diffs-for-eagle-and-git
+# https://github.com/hurik/visual-diffs-for-eagle-and-git
+#
+# v0.1.0
+#
+# Created by Andreas Giemza on 2012-06-14.
+#
+# Based on: https://gitorious.org/gitedaous/eagle-converter
+#           http://jeffkreeftmeijer.com/2011/comparing-images-and-creating-image-diffs/
+
+require 'grit'
+require 'chunky_png'
+include ChunkyPNG::Color
+
+# Oprions
+repoPath = "e:/visual-diffs-for-eagle-and-git_testrepo"
+repoBranch = "master"
+@eaglePath = "c:/Programme/EAGLE-6.1.0/bin/eagle.exe"
+
+
+def parseTree(tree)
+	tree.contents.each do |content|
+		if content.kind_of? Grit::Blob
+			type = content.name.split('.').last
+			if type == 'sch' or type == 'brd'
+				puts "Got EAGLE file: #{content.name}"
+
+				temp_data = @tempPath + '/' + content.name
+
+				file = File.new(temp_data, "w")
+				file.puts(content.data)
+            	file.close
+
+            	`#{@eaglePath} -C 'RUN #{@currentPath}/countSheets.ulp #{@tempPath}/sheet_info.txt; QUIT' #{temp_data}`
+            	
+				sheetCount = IO.read("#{@tempPath}/sheet_info.txt").to_i
+
+	            if sheetCount == 0
+	            	`#{@eaglePath} -C 'EXPORT IMAGE #{@commitPath}/#{content.name}.png 150; QUIT' #{temp_data}`
+	            else
+	            	sheetCount.times do |i|
+	            	`#{@eaglePath} -C 'EDIT .s#{i+1}; EXPORT IMAGE #{@commitPath}/#{content.name}_#{i+1}.png 150; QUIT' #{temp_data}`
+	            	end
+	            end
+
+            	File.delete(temp_data)
+			end
+		end
+	end
+end
+
+puts "Initializing"
+@currentPath = Dir.getwd
+@tempPath = @currentPath + "/temp"
+@imagesPath = @currentPath + "/images"
+@commitImagesPath = @imagesPath + "/commits"
+@diffImagesPath = @imagesPath + "/diff"
+
+if !File.directory? (@tempPath)
+	puts "Creating temp directory"
+	Dir.mkdir(@tempPath)
+end
+
+if !File.directory? (@imagesPath)
+	puts "Creating images directory"
+	Dir.mkdir(@imagesPath)
+end
+
+if !File.directory? (@commitImagesPath)
+	puts "Creating images/commits directory"
+	Dir.mkdir(@commitImagesPath)
+end
+
+if !File.directory? (@diffImagesPath)
+	puts "Creating images/commits directory"
+	Dir.mkdir(@diffImagesPath)
+end
+
+puts "Opening repository at #{repoPath} with branch #{repoBranch}"
+repo = Grit::Repo.new(repoPath)
+
+commitCounter = 0
+
+repo.commits(repoBranch).reverse_each do |commit|
+	commitCounter += 1
+
+	@commitPath = @commitImagesPath + "/#{commitCounter}"
+
+	if File.directory? (@commitPath)
+		puts "Already parsed #{commit.id}"
+	else
+		puts "Parsing tree #{commit.id}"
+		Dir.mkdir(@commitPath)
+		parseTree(commit.tree)
+	end
+end
+
+commitCounter = 0
+
+loop do
+	commitCounter += 1
+
+	commitPath1 = "#{@commitImagesPath}/#{commitCounter}"
+
+	if File.directory? (commitPath1)
+		commitPath2 = "#{@commitImagesPath}/#{commitCounter+1}"
+
+		if File.directory? (commitPath2)
+			for file in Dir.entries(commitPath1)
+				if !(file == "." || file == "..")
+					if File.exist? ("#{commitPath2}/#{file}")
+
+						diffImageFolder = "#{@diffImagesPath}/#{file}"
+
+						if !File.directory? (diffImageFolder)
+							puts "Creating images/commits directory"
+							Dir.mkdir(diffImageFolder)
+						end						
+
+						diffImageFile = "#{diffImageFolder}/#{commitCounter}_#{commitCounter+1}_#{file}"
+
+						if !File.exist? (diffImageFile)
+							puts "Creating #{commitCounter}_#{commitCounter+1}_#{file}"
+
+							images = [
+							  ChunkyPNG::Image.from_file("#{commitPath1}/#{file}"),
+							  ChunkyPNG::Image.from_file("#{commitPath2}/#{file}")
+							]
+
+							output = ChunkyPNG::Image.new(images.first.width, images.last.width, WHITE)
+
+							diff = []
+
+							images.first.height.times do |y|
+							  images.first.row(y).each_with_index do |pixel, x|
+							    unless pixel == images.last[x,y]
+							      score = Math.sqrt(
+							        (r(images.last[x,y]) - r(pixel)) ** 2 +
+							        (g(images.last[x,y]) - g(pixel)) ** 2 +
+							        (b(images.last[x,y]) - b(pixel)) ** 2
+							      ) / Math.sqrt(MAX ** 2 * 3)
+
+							      output[x,y] = grayscale(MAX - (score * MAX).round)
+							      diff << score
+							    end
+							  end
+							end
+
+							output.save(diffImageFile)
+						else
+							puts "Already created #{commitCounter}_#{commitCounter+1}_#{file}"
+						end
+					end
+				end
+			end
+		else
+			break
+		end
+	else
+		break
+	end
+end
