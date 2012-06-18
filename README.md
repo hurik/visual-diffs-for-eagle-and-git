9713d3a1b9b48745f3e2495c58eedf5b87179bcf
Andreas Giemza <andreas@giemza.net>
2012-06-18 03:09:32 +0200

    v0.2.0 - important fixes with subfolders and the diff images (and more)

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ If you are working on an [CadSoft EAGLE](http://www.cadsoftusa.com/eagle-pcb-des
 
 But first some notes:
 * Tested under Windows and Linux (Ubuntu).
-* Schematics and Boards must always have different names, also when they are in different directories.
 * The files are not allowed to have spaces in their name.
 * This my first ruby programm. It is very, very ugly written.
 * Sorry for my bad English!
@@ -40,41 +39,46 @@ He made the countSheets.ulp file and most of the code is from him, but i didn't 
 ## Usage
 1. Open the vdfeag.rb file and set the repoPath, repoBranch and @eaglePath.
 1. Start the file!
-1. After some time your find the diff images under images/diff.
+1. After some time your find the diff images under diff for each file in the repo.
+1. When a new commit was added, you can rerun the programm and it's only makes the diff images for the new commit.
+
+**Important:** When you want to get diff images of another repo, you have to delete the diff and temp folder!
 
 
 ## TODO
-* **Improve code**
-* Improve speed more
-* Improve folder structure
-* Improve file naming
-* Improve diff images
-    * Board diffs are very ugly ...
-* Improve comments
-* Improve programm output
-* Improve everything ...
-* Add a gui
-
-
-## DONE
-* Improve speed (v0.1.2)
-* Improve diff images (v0.1.3)
-    * Removed elements should be red (v0.1.3)
-    * Added elements should be green (v0.1.3)
-    * Unchanged elements should be brightened (v0.1.3)
+* Important fixes/improvements
+	* Allow spaces in filenames and folders
+	* Improve board diffs (Not sure if it should make images of all layers)
+	* Only make an diff image if there was a change (That would safe some time)
+* Minor fixes/improvements
+	* Improve code
+	* Improve speed
+	* Improve folder structure (Not sure it it would be better to get all diff images of one commit in one folder or keep the current structure)
+	* Improve file naming
+	* Improve comments
+* fixes/improvements for the future
+	* Add a gui (Where you can choose the commits and files of which you want the diff image)
 
 
 ## Changelog
+### v0.2.0
+* **Subfolders are now included in the name (Files with same name in different folders are now working)**
+* **Diff images now working when the images size is not the same**
+* Code was cleaned a lot
+* Programm output now without useless information
+* Improved folder structure
+* Improve file naming
+
 #### v0.1.3
-* Highly improved diff images
+* **Highly improved diff images**
 * Fixed little bug with image size
 * Example image added
 
 #### v0.1.2
-* Massiv speedup with oily_png. Now about 14 times faster!
+* **Massiv speedup with oily_png. Now about 14 times faster!**
 
 #### v0.1.1
-* Now it is proccessing files in subfolders
+* **Now it is proccessing files in subfolders**
 
 ### v0.1.0
 * First commit
--- vdfeag.rb
+++ vdfeag.rb
@@ -1,9 +1,9 @@
 # visual-diffs-for-eagle-and-git
 # https://github.com/hurik/visual-diffs-for-eagle-and-git
 #
-# v0.1.3
+# v0.2.0
 #
-# Created by Andreas Giemza on 2012-06-14.
+# Created by Andreas Giemza on 2012-06-17.
 #
 # Based on: https://gitorious.org/gitedaous/eagle-converter
 #           http://jeffkreeftmeijer.com/2011/comparing-images-and-creating-image-diffs/
@@ -12,72 +12,131 @@ require 'grit'
 require 'oily_png'
 include ChunkyPNG::Color
 
-# Oprions
+# Options
 repoPath = "e:/visual-diffs-for-eagle-and-git_testrepo"
 repoBranch = "master"
 @eaglePath = "c:/Programme/EAGLE-6.1.0/bin/eagle.exe"
 
-
-def parseTree(tree)
-	tree.contents.each do |content|
-		if content.kind_of? Grit::Blob
-			type = content.name.split('.').last
-			if type == 'sch' or type == 'brd'
-				puts "Got EAGLE file: #{content.name}"
-
-				temp_data = @tempPath + '/' + content.name
-
-				file = File.new(temp_data, "w")
-				file.puts(content.data)
-            	file.close
-
-            	`#{@eaglePath} -C 'RUN #{@currentPath}/countSheets.ulp #{@tempPath}/sheet_info.txt; QUIT' #{temp_data}`
-            	
-				sheetCount = IO.read("#{@tempPath}/sheet_info.txt").to_i
-
-	            if sheetCount == 0
-	            	`#{@eaglePath} -C 'EXPORT IMAGE #{@commitPath}/#{content.name}.png 150; QUIT' #{temp_data}`
-	            else
-	            	sheetCount.times do |i|
-	            	`#{@eaglePath} -C "EDIT .s#{i+1}; EXPORT IMAGE #{@commitPath}/#{content.name}_#{i+1}.png 150; QUIT" #{temp_data}`
-	            	end
-	            end
-
-            	File.delete(temp_data)
-			end
-		end
-		if content.kind_of? Grit::Tree
-			parseTree(content)
-		end
-	end
+# Checks if a directory exists and when not, it creates it
+def createDirectory(path)
+  if !File.directory? (path)
+    puts "Creating #{path} directory"
+    Dir.mkdir(path)
+  end
 end
 
-puts "Initializing"
+# Creates the diff image
+def createDiffImage(oldImage, newImage, diffImage)
+  images = [
+    ChunkyPNG::Image.from_file(oldImage),
+    ChunkyPNG::Image.from_file(newImage)
+  ]
+
+  width = [images.first.width, images.last.width].max
+  height = [images.first.height, images.last.height].max
+
+  if images.first.width != width or images.first.height != height
+    oldTemp = ChunkyPNG::Image.new(width, height, WHITE)
+
+    images.first.height.times do |y|
+      images.first.row(y).each_with_index do |pixel, x|
+        oldTemp[x,y] = pixel
+      end
+    end
+
+    images[0] = oldTemp
+  end
+
+  if images.last.width != width or images.last.height != height
+    newTemp = ChunkyPNG::Image.new(width, height, WHITE)
+
+    images.last.height.times do |y|
+      images.last.row(y).each_with_index do |pixel, x|
+        newTemp[x,y] = pixel
+      end
+    end
+
+    images[1] = newTemp
+  end
+
+  output = ChunkyPNG::Image.new(width, height, WHITE)
+
+  images.first.height.times do |y|
+    images.first.row(y).each_with_index do |pixel, x|
+      if pixel == images.last[x,y]
+        if pixel == WHITE
+          next
+        else
+          output[x,y] = interpolate_quick(pixel, WHITE, 80)
+        end
+      elsif pixel == WHITE
+        output[x,y] = :green
+      elsif images.last[x,y] == WHITE
+        output[x,y] = :red
+      else
+        output[x,y] = :grey
+      end
+    end
+  end
+
+  output.save(diffImage)
+end
+
+# Parse the commits
+def parseTree(tree, currentFolder)
+  if tree.name != nil
+    currentFolder = currentFolder + tree.name.to_s + "_-_"
+  end
+
+  tree.contents.each do |content|
+    if content.kind_of? Grit::Blob
+      type = content.name.split('.').last
+      if type == 'sch' or type == 'brd'
+        puts "Got EAGLE file: #{content.name}"
+
+        temp_data = @tempPath + '/' + content.name
+
+        file = File.new(temp_data, "w")
+        file.puts(content.data)
+        file.close
+
+        `#{@eaglePath} -C 'RUN #{@currentPath}/countSheets.ulp #{@tempPath}/sheet_info.txt; QUIT' #{temp_data}`
+
+        sheetCount = IO.read("#{@tempPath}/sheet_info.txt").to_i
+
+        if sheetCount == 0
+          `#{@eaglePath} -C 'EXPORT IMAGE #{@commitPath}/#{currentFolder}#{content.name}.png 150; QUIT' #{temp_data}`
+        else
+          sheetCount.times do |i|
+            `#{@eaglePath} -C "EDIT .s#{i+1}; EXPORT IMAGE #{@commitPath}/#{currentFolder}#{content.name}_page#{i+1}.png 150; QUIT" #{temp_data}`
+          end
+        end
+
+        File.delete(temp_data)
+      end
+    end
+    if content.kind_of? Grit::Tree
+      parseTree(content, currentFolder)
+    end
+  end
+end
+
+puts "\nInitializing"
+puts "------------\n"
+
 @currentPath = Dir.getwd
 @tempPath = @currentPath + "/temp"
-@imagesPath = @currentPath + "/images"
-@commitImagesPath = @imagesPath + "/commits"
-@diffImagesPath = @imagesPath + "/diff"
+@commitImagesPath = @tempPath + "/commits"
+@diffImagesPath = @currentPath + "/diffs"
 
-if !File.directory? (@tempPath)
-	puts "Creating temp directory"
-	Dir.mkdir(@tempPath)
-end
+createDirectory(@tempPath)
+createDirectory(@commitImagesPath)
+createDirectory(@diffImagesPath)
 
-if !File.directory? (@imagesPath)
-	puts "Creating images directory"
-	Dir.mkdir(@imagesPath)
-end
+puts "Done!"
 
-if !File.directory? (@commitImagesPath)
-	puts "Creating images/commits directory"
-	Dir.mkdir(@commitImagesPath)
-end
-
-if !File.directory? (@diffImagesPath)
-	puts "Creating images/commits directory"
-	Dir.mkdir(@diffImagesPath)
-end
+puts "\nParsing repo"
+puts "------------\n"
 
 puts "Opening repository at #{repoPath} with branch #{repoBranch}"
 repo = Grit::Repo.new(repoPath)
@@ -85,82 +144,50 @@ repo = Grit::Repo.new(repoPath)
 commitCounter = 0
 
 repo.commits(repoBranch).reverse_each do |commit|
-	commitCounter += 1
+  commitCounter += 1
 
-	@commitPath = @commitImagesPath + "/#{commitCounter}"
+  @commitPath = @commitImagesPath + "/#{commitCounter}"
 
-	if File.directory? (@commitPath)
-		puts "Already parsed #{commit.id}"
-	else
-		puts "Parsing tree #{commit.id}"
-		Dir.mkdir(@commitPath)
-		parseTree(commit.tree)
-	end
+  if !File.directory? (@commitPath)
+    puts "Parsing tree #{commit.id}"
+    createDirectory(@commitPath)
+    parseTree(commit.tree, "")
+  end
 end
+
+puts "Done!"
+
+puts "\nDiff images"
+puts "-----------\n"
 
 commitCounter = 0
 
 loop do
-	commitCounter += 1
+  commitCounter += 1
 
-	commitPath1 = "#{@commitImagesPath}/#{commitCounter}"
+  commitPath1 = "#{@commitImagesPath}/#{commitCounter}"
+  commitPath2 = "#{@commitImagesPath}/#{commitCounter+1}"
 
-	if File.directory? (commitPath1)
-		commitPath2 = "#{@commitImagesPath}/#{commitCounter+1}"
+  if File.directory? (commitPath1) and File.directory? (commitPath2)
+    for file in Dir.entries(commitPath1)
+      if !(file == "." || file == "..") and File.exist? ("#{commitPath2}/#{file}")
 
-		if File.directory? (commitPath2)
-			for file in Dir.entries(commitPath1)
-				if !(file == "." || file == "..")
-					if File.exist? ("#{commitPath2}/#{file}")
+        diffImageFolder = "#{@diffImagesPath}/#{file.chomp(".png")}"
 
-						diffImageFolder = "#{@diffImagesPath}/#{file}"
+        createDirectory(diffImageFolder)
 
-						if !File.directory? (diffImageFolder)
-							puts "Creating images/commits directory"
-							Dir.mkdir(diffImageFolder)
-						end						
+        diffImageFile = "#{diffImageFolder}/#{commitCounter}_#{commitCounter+1}_#{file}"
 
-						diffImageFile = "#{diffImageFolder}/#{commitCounter}_#{commitCounter+1}_#{file}"
+        if !File.exist? (diffImageFile)
+          puts "Creating diff image: #{commitCounter}_#{commitCounter+1}_#{file}"
 
-						if !File.exist? (diffImageFile)
-							puts "Creating #{commitCounter}_#{commitCounter+1}_#{file}"
-
-							images = [
-							  ChunkyPNG::Image.from_file("#{commitPath1}/#{file}"),
-							  ChunkyPNG::Image.from_file("#{commitPath2}/#{file}")
-							]
-
-							output = ChunkyPNG::Image.new(images.first.width, images.first.height, WHITE)
-
-							images.first.height.times do |y|
-							  images.first.row(y).each_with_index do |pixel, x|
-							    if pixel == images.last[x,y]
-							      if pixel == WHITE
-							        next
-							      else
-							        output[x,y] = interpolate_quick(pixel, WHITE, 80)
-							      end
-							    elsif pixel == WHITE
-							      output[x,y] = :green
-							    elsif images.last[x,y] == WHITE
-							      output[x,y] = :red
-							    else
-							      output[x,y] = :grey
-							    end
-							  end
-							end
-
-							output.save(diffImageFile)
-						else
-							puts "Already created #{commitCounter}_#{commitCounter+1}_#{file}"
-						end
-					end
-				end
-			end
-		else
-			break
-		end
-	else
-		break
-	end
+          createDiffImage("#{commitPath1}/#{file}", "#{commitPath2}/#{file}", diffImageFile)
+        end
+      end
+    end
+  else
+    break
+  end
 end
+
+puts "Done!"
